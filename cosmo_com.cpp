57a58aa1476fe7a3c7e86bc70543e96eea6aba78
Lu Yuxun <lu-yuxun@nii.ac.jp>
2020-11-03 22:06:56 +0900

    Dependency of "single header JSON" added into the project.

--- cosmo_com.cpp
+++ cosmo_com.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <nlohmann/json.hpp>
+typedef nlohmann::json config;
 int main(int argc, char** argv)
 {
+    // Default configuration file operator.
+    // 默认配置文件算子.
+    config config_operator;
 }
